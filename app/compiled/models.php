d7c8f4c97dee4f356b8a172ea92018409d8216a5
unknown <vferrero14@gmail.com>
2016-03-17 21:32:30 -0500

    skp

--- app/compiled/models.php
+++ app/compiled/models.php
@@ -544,10 +544,10 @@ class DB_Connections
 			 $success = true;
 		 } catch(Exception $e) {
 			 $success = false;
-		//	 $arrReturn['error'] = $e->getMessage();
+			 $arrReturn['error'] = $e->getMessage();
 		 }
-	//	 $arrReturn['success'] = $success;
-	//	 $arrReturn['DBO'] = $db;
+		 $arrReturn['success'] = $success;
+		 $arrReturn['DBO'] = $db;
 		 return $db;
 	}	
 }
@@ -570,6 +570,8 @@ class MuseumModel
 	
 
 	public function getEntireMuseum($id) {
+		return $this-dbo;
+		/*
 		$arrResult = array();
 		$success = false;
 		 try {
@@ -611,6 +613,7 @@ class MuseumModel
 		}
 		$arrResult['success'] = $success;
 		return $arrResult;
+		*/
 	}
 	
 	/**
@@ -1214,4 +1217,33 @@ class UserModel{
 	}
 }
 
+?>
+<?php
+
+
+class DatabaseConnectionStrings{
+
+	private static $MySQL_DSCS = "localhost:root:r00t:Virgil"; // TODO: need to get MySql connection string
+	
+	// just default to MySql right now
+	public static function getDBCS($dbNickname) {		
+	//	if(strcmp($dbNickname, "MySQL") == 0) {
+			return DatabaseConnectionStrings::$MySQL_DSCS;
+	//	}
+	}
+	
+	public static function getDBCredentials($environment){
+		$environment = 'local';
+		//TODO: get nickname from environment->nickname mapping (maybe assoc array)?
+		$numericalArr = explode(':',DatabaseConnectionStrings::getDBCS("MySQL_local"));
+		$optionsArr = array();
+		return array("host"=>$numericalArr[0],
+					"username"=>$numericalArr[1],
+					"password"=>$numericalArr[2],
+					"schema"=>$numericalArr[3],
+					"dsn"=>'mysql:host='.$numericalArr[0].';dbname='.$numericalArr[3].'',
+					"options"=>$optionsArr);
+	}
+
+}
 ?>
