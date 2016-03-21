b8a978d37ed623faf611438a1015a1c29f57c7ae
unknown <vferrero14@gmail.com>
2016-03-21 18:44:41 -0500

    trying to handle image uploads. Testing how to create a new directory programatically on the server

--- app/compiled/models.php
+++ app/compiled/models.php
@@ -641,6 +641,7 @@ class MuseumModel
 	public function createContent() {
 		// TODO: need to mess around with uploading images first.
 		// will need to write some test scripts so i get the hang of it
+		$this->handleUploadedImage(10);
 		return "createContent funtion not implemented yet";
 	}
 
@@ -651,212 +652,65 @@ class MuseumModel
 	public function deleteContent() {
 		return "deleteContent funtion not implemented yet";
 	}
-}
 
-?>
-<?php
-/*
- * Org table database schema: 
- * 
- * id | name | address | city | state | zip | phone | email | phone2 | profileJSON
- * 
- * */
-class OrgModel{
-		private $dbo;
-
-	public function __construct() {
-		$db = new DB_Connections();
-		$this->dbo = $db->getNewDBO();
-	 }
-	 public function __destruct() {
-		$this->dbo = null;
+	private function handleUploadedImage($museumId) {
+		$target_dir = "uploads/images/" . $museumId . "/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		if (!is_dir($target_dir)) {
+   			 mkdir($target_dir, 0777, true);
+		}
+		/*
+		// Check if image file is a actual image or fake image
+		if(isset($_POST["submit"])) {
+		    $check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
+		    if($check !== false) {
+		      //  echo "File is an image - " . $check["mime"] . ".";
+		        $uploadOk = 1;
+		    } 
+		    else {
+		     //   echo "File is not an image.";
+		        $uploadOk = 0;
+		    }
+		}
+		// Check if file already exists
+		if (file_exists($target_file)) {
+		   // echo "Sorry, file already exists.";
+		    $uploadOk = 0;
+		}
+		// Check file size
+		if ($_FILES["fileToUpload"]["size"] > 500000) {
+		  //  echo "Sorry, your file is too large.";
+		    $uploadOk = 0;
+		}
+		// Allow certain file formats
+		if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+		&& $imageFileType != "gif" ) {
+		 //   echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+		    $uploadOk = 0;
+		}
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+		    echo "Sorry, your file was not uploaded.";
+		    $success = false;
+		// if everything is ok, try to upload file
+		} 
+		else {
+		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		      //  echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+		    	$success = true;
+		    } 
+		    else {
+		      //  echo "Sorry, there was an error uploading your file.";
+		    	$success = false;
+		    }
+		}
+		return $success;
+		*/
 	}
-	
-	
-	 /**
-		expected input: 
-		$arrValues => assoc array containing all fields neccessary 
-					to add an org to the database
-		output:
-		$arrResult = array (
-		'error' => exception object for db query
-		'success' => true if org was successfully retrieved from the db
-		);
-	*/
-	 public function createOrg($arrValues) {
-		$arrResult = array();
-		$success = false;		
-		$name = $arrValues['name'];
-		$address = $arrValues['address'];
-	    $city = $arrValues['city'];
-		$state = $arrValues['state'];
-		$zip = $arrValues['zip'];
-		$phone = $arrValues['phone'];
-		$email = $arrValues['email'];
-		$phone2 = $arrValues['phone2'];
-		$profileJSON = $arrValues['profileJSON'];
-		 try {
-			$data = array( 
-			'name' => $name, 'address' => $address, 
-			'city' => $city, 'state' => $state, 
-			'zip' => $zip, 'phone' => $phone, 'email' => $email,
-			'phone2' => $phone2, 'profileJSON' => $profileJSON);
-			$STH = $this->dbo->prepare("INSERT INTO org VALUES (NULL, :name, :address, 
-			:city, :state, :zip, :phone, :email, :phone2, :profileJSON)");
-			$STH->execute($data);
-			$success = true;
-		} catch (Exception $e) {
-			$success = false;
-			$arrResult['error'] = $e->getMessage();
-		}
-		$arrResult['success'] = $success;
-		return $arrResult;
-	 }
-	 
-	  /**
-		expected input: 
-		$arrValues => assoc array containing all fields to be changed.
-					if a field is not being changed, it must be set to empty string
-		output:
-		$arrResult = array (
-		'error' => exception object for db query
-		'success' => true if org was successfully retrieved from the db
-		);
-	*/
-	 public function editOrg($arrValues) {
-	$arrResult = array();
-	$success = false;
-	$id = $arrValues['id'];
-	$name = $arrValues['name'];
-	$address = $arrValues['address'];
-	$city = $arrValues['city'];
-	$state = $arrValues['state'];
-	$zip = $arrValues['zip'];
-	$phone = $arrValues['phone'];
-	$email = $arrValues['email'];
-	$phone2 = $arrValues['phone2'];
-	$profileJSON = $arrValues['profileJSON'];
-	 $sql = "UPDATE org SET ";
-	 $data = array();
-	 $index = 0;
-	 if(strcmp($name, "") != 0) {
-		 $sql = $sql . "name=?, ";
-		 $data[$index] = $name;
-		 $index = $index + 1;
-	 }
-	 if(strcmp($address, "") != 0) {
-		 $sql = $sql . "address=?, ";
-		 $data[$index] = $address;
-		 $index = $index + 1;
-	 }
-	 if(strcmp($city, "") != 0) {
-		 $sql = $sql . "city=?, ";
-		 $data[$index] = $city;
-		 $index = $index + 1;
-	 }
-	 if(strcmp($state, "") != 0) {
-		 $sql = $sql . "state=?, ";
-		 $data[$index] = $state;
-		 $index = $index + 1;
-	 }
-	  if(strcmp($zip, "") != 0) {
-		 $sql = $sql . "zip=?, ";
-		 $data[$index] = $zip;
-		 $index = $index + 1;
-	 }
-	  if(strcmp($phone, "") != 0) {
-		 $sql = $sql . "phone=?, ";
-		 $data[$index] = $phone;
-		 $index = $index + 1;
-	 }
-	  if(strcmp($email, "") != 0) {
-		 $sql = $sql . "email=?, ";
-		 $data[$index] = $email;
-		 $index = $index + 1;
-	 }
-	  if(strcmp($phone2, "") != 0) {
-		 $sql = $sql . "phone2=?, ";
-		 $data[$index] = $phone2;
-		 $index = $index + 1;
-	 }
-	  if(strcmp($profileJSON, "") != 0) {
-		 $sql = $sql . "profileJSON=?, ";
-		 $data[$index] = $profileJSON;
-		 $index = $index + 1;
-	 }
-	 // get rid of the last two characters
-	 $sql = substr($sql,0,-2);
-	 $sql = $sql . " WHERE id=?";
-	 $data[$index] = $id;
-	try {
-		 $stm = $this->dbo->prepare($sql);
-		 $arrResult['db_result'] = $stm->execute($data);
-		 $success = true;
-     } catch (Exception $e) {
-		 $arrResult['error'] = $e->getMessage();
-		 $success = false;
-	 }	
-	$arrResult['success'] = $success;
-	$arrResult['sql'] = $sql;
-	$arrResult['data'] = $data;
-	return $arrResult;
-	 }
-	 
-	 /**
-		expected input: 
-		$id => the id of the org being deleted
-		output:
-		$arrResult = array (
-		'error' => exception object for db query
-		'success' => true if org was successfully retrieved from the db
-		'db_result' => result of running the query. 1 for success, 0 for failure
-		);
-	*/	
-	 public function deleteOrg($id) {
-		$arrResult = array();
-		$success = false;
-		$sql = "DELETE FROM org WHERE id=:id";
-		try {
-			$stm = $this->dbo->prepare($sql);
-			$stm->bindParam(":id", $id);
-			$arrResult['db_result'] = $stm->execute();
-			$success = true;
-		} catch(Exception $e) {
-			$arrResult['error'] = $e->getMessage();
-			$success = false;
-		}
-		$arrResult['success'] = $success;
-		return $arrResult;
-	 }
-	 
-	 /**
-		expected input: the orgs id
-		output:
-		$arrResult = array (
-		'data' => assoc array containing this orgs record from db
-		'error' => exception object for db query
-		'success' => true if org was successfully retrieved from the db
-		);
-	*/
-	 public function getOrgById($id) {
-		 $success = false;
-		 $arrResult = array();
-		 
-		 try {
-			$stmt = $this->dbo->prepare("SELECT * FROM org WHERE id=:id");
-			$stmt->bindParam(':id', $id);
-			$stmt->execute();
-			$row = $stmt->fetch(PDO::FETCH_ASSOC);
-			$success = true;
-			$arrResult['data'] = $row; 
-		 } catch(Exception $e) {
-			 $success = false;
-			 $arrResult['error'] = $e->getMessage();
-		 }
-		 $arrResult['success'] = $success;
-		 return $arrResult;
-	 }
 }
+
 ?>
 <?php
 	// id | username | password | email | userRole | orgId
