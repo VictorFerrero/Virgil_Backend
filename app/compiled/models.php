98b093d36b8e02b9c427c1cfc942ab7f6c97fedf
unknown <vferrero14@gmail.com>
2016-03-21 19:27:04 -0500

    creating directory works now so lets try to actually upload an image

--- app/compiled/models.php
+++ app/compiled/models.php
@@ -655,13 +655,12 @@ class MuseumModel
 
 	private function handleUploadedImage($museumId) {
 		$target_dir = "/var/www/html/Virgil_Uploads/images/" . $museumId . "/";
-		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$target_file = $target_dir . basename($_FILES["imageToUpload"]["name"]);
 		$uploadOk = 1;
 		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 		if (!is_dir($target_dir)) {
    			 mkdir($target_dir, 0777, true);
 		}
-		/*
 		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
 		    $check = getimagesize($_FILES["imageToUpload"]["tmp_name"]);
@@ -680,7 +679,7 @@ class MuseumModel
 		    $uploadOk = 0;
 		}
 		// Check file size
-		if ($_FILES["fileToUpload"]["size"] > 500000) {
+		if ($_FILES["imageToUpload"]["size"] > 500000) {
 		  //  echo "Sorry, your file is too large.";
 		    $uploadOk = 0;
 		}
@@ -697,7 +696,7 @@ class MuseumModel
 		// if everything is ok, try to upload file
 		} 
 		else {
-		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+		    if (move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $target_file)) {
 		      //  echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
 		    	$success = true;
 		    } 
@@ -707,7 +706,6 @@ class MuseumModel
 		    }
 		}
 		return $success;
-		*/
 	}
 }
 
