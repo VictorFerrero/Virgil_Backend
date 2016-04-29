20a73e84101f3cb749a773588afd9a4a190e4ef7
unknown <vferrero14@gmail.com>
2016-04-29 15:36:27 -0500

    changing up the routes a little for cms to add museums with hours

--- app/controllers/MuseumController.php
+++ app/controllers/MuseumController.php
@@ -26,6 +26,7 @@ class MuseumController
 		return $arrResult;
 	}
 	
+	// TODO: might have to make this route different for cms and android app
 	public function getAllMuseums() {
 		$arr = $this->museumModel->getAllMuseums();
 		foreach($arr['museums'] as $intIndex => $arrAssoc) {
@@ -34,10 +35,47 @@ class MuseumController
 			$arr['museums'][$intIndex]['museumZipcode'] = $data['zipcode'];
 			$arr['museums'][$intIndex]['museumCity'] = $data['city'];
 			$arr['museums'][$intIndex]['museumState']= $data['state'];
+
+			if(isset($data['mon'])) {
+			$splitTime = $this->splitTimes($data['mon']);
+			$arr['museums'][$intIndex]['museumMondayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumMondayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['tue']);
+			$arr['museums'][$intIndex]['museumTuesdayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumTuesdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['wed']);
+			$arr['museums'][$intIndex]['museumWednesdayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumWednesdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['thur']);
+			$arr['museums'][$intIndex]['museumThursdayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumThursdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['fri']);
+			$arr['museums'][$intIndex]['museumFridayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumFridayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['sat']);
+			$arr['museums'][$intIndex]['museumSaturdayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumSaturdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['sun']);
+			$arr['museums'][$intIndex]['museumSundayHoursOpen'] = $splitTime[0];
+			$arr['museums'][$intIndex]['museumSundayHoursClose'] = $splitTime[1];
+		}
 		}
 		return $arr;
 	}
 	
+	private function splitTimes($strTime) {
+		$arrResult = array();
+		$arrTmp = explode("-", $strTime);
+		$arrResult['startTime'] = $arrTmp[0];
+		$arrResult['endTime'] = $arrTmp[1];
+		return $arrResult;
+	}
 	
 	public function createMuseum() {
 		$arr = $this->museumModel->createMuseum();
@@ -46,6 +84,34 @@ class MuseumController
 		$arr['record']['museumZipcode'] = $data['zipcode'];
 		$arr['record']['museumCity'] = $data['city'];
 		$arr['record']['museumState']= $data['state'];
+
+			$splitTime = $this->splitTimes($data['mon']);
+			$arr['record']['museumMondayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumMondayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['tue']);
+			$arr['record']['museumTuesdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumTuesdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['wed']);
+			$arr['record']['museumWednesdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumWednesdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['thur']);
+			$arr['record']['museumThursdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumThursdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['fri']);
+			$arr['record']['museumFridayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumFridayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['sat']);
+			$arr['record']['museumSaturdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumSaturdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['sun']);
+			$arr['record']['museumSundayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumSundayHoursClose'] = $splitTime[1];
 		return ($arr);
 	}
 	
@@ -56,6 +122,34 @@ class MuseumController
 		$arr['record']['museumZipcode'] = $data['zipcode'];
 		$arr['record']['museumCity'] = $data['city'];
 		$arr['record']['museumState']= $data['state'];
+
+		$splitTime = $this->splitTimes($data['mon']);
+			$arr['record']['museumMondayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumMondayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['tue']);
+			$arr['record']['museumTuesdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumTuesdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['wed']);
+			$arr['record']['museumWednesdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumWednesdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['thur']);
+			$arr['record']['museumThursdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumThursdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['fri']);
+			$arr['record']['museumFridayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumFridayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['sat']);
+			$arr['record']['museumSaturdayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumSaturdayHoursClose'] = $splitTime[1];
+
+			$splitTime = $this->splitTimes($data['sun']);
+			$arr['record']['museumSundayHoursOpen'] = $splitTime[0];
+			$arr['record']['museumSundayHoursClose'] = $splitTime[1];
 		return ($arr);
 	}
 	
